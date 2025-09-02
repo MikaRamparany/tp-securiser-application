c8be041d2e2df228d097bbacf1f1324421483a46
MikaRamparany <migame.strass@gmail.com>
2025-09-02 10:24:58 +0200

    III. Fichier configuration : connect.php étant codé en dure, je dois sécuriser et mettre en place un fichier de config avec des constantes utilisable dans le code. Fait : Créé Dossier/File -> config/config.php avec la configuration dedans, puis l'appel (require dans connect.php) pour pouvoir utiliser les constantes.

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+// Attention : Je ne dois pas commiter de vrais 'secrets', ici je fais ça dans le cadre de mon TP
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'phpsec');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+?>
--- connect.php
+++ connect.php
@@ -1,3 +1,14 @@
 <?php
-	$db = new PDO('mysql:host=localhost;dbname=phpsec', 'root', '');
-?>
+require_once __DIR__.'/config/config.php';
+
+$db = new PDO(
+  'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8mb4',
+  DB_USER,
+  DB_PASS,
+  [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Gestion des erreurs qui sert à afficher les erreurs SQL
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Gestion du mode de récupération des données
+  ]
+  // les deux lignes ci-dessous me permettent de gérer les erreurs et de récupérer les données sous forme de tableau associatif
+);
+> 
